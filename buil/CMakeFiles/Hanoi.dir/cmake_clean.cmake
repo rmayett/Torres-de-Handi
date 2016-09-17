071c79dcad15934a276ae0ad340b2b28b965d535
rodrigo <rmayett@comunidad.unam.mx>
2016-09-17 13:45:16 -0500

    Se remplazo el proyecto entero por el proporcionado por el profesor

---
+++ buil/CMakeFiles/Hanoi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hanoi.dir/src/Hanoi.cpp.o"
+  "CMakeFiles/Hanoi.dir/src/main.cpp.o"
+  "Hanoi.pdb"
+  "Hanoi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hanoi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
